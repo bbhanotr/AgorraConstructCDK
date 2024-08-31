4d9e1065d199df9984409c300ab7d35acbb56f03
Rishabh Bhanot <rishabhbhanot@Rishabhs-MacBook-Pro.local>
2024-08-31 16:58:57 +0530

    Initial commit

---
+++ test/agorra_construct_cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AgorraConstructCdk = require('../lib/agorra_construct_cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/agorra_construct_cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AgorraConstructCdk.AgorraConstructCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
